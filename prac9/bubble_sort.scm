94c6842a25212362d3a2a5a93b0341c7bc3ece08
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-10-23 10:52:52 +0800

    feat: add functional bubblesort

--- prac9/bubble_sort.scm
+++ prac9/bubble_sort.scm
@@ -1,3 +1,4 @@
+; Returns true if input list is sorted and false otherwise
 (define (sorted list)
     (cond 
         ; If we reached the end of the list, we are sorted
@@ -5,33 +6,55 @@
             #t
         )
         ; If these were sorted, try the next
-        ((<= (car list) (cdar list))
+        ((<= (car list) (cadr list))
             (sorted (cdr list))
         )
         ; If not sorted, return false
-        ((> (cdar list) (car list))
+        ((> (car list) (cadr list))
             #f
         ) 
     )
 )
 
+; Performs bubble-sort on the imported list
 (define (bubble-sort list)
     (cond
-        ((sorted list) 
+        ; If we are sorted, we are done
+        ((sorted list)
             list
         ) 
+        ; If we are not sorted, do another run
         ((not (sorted list))
             (bubble-sort (bubble-sort-run list))
         ) 
     )
 )
 
+; Performs one run (trickle) of bubble sort on the imported list
 (define (bubble-sort-run list)
     (cond
-        ((equals? (car list) '())
+        ; If we've reached the end of the list, recursively return the list
+        ((equal? (cdr list) '())
             list
         )
-        ((> (car list) )
+        ; If next two elements are the wrong way around, swap them and continue
+        ((> (car list) (cadr list))
+            (cons (car (swap-first list)) (bubble-sort-run (cdr (swap-first list))))
+        )
+        ; If next two elements are right way around, don't swap them and continue
+        ((<= (car list) (cadr list))
+            (cons (car list) (bubble-sort-run (cdr list)))
         )
     )
 )
+
+; Swaps the first and second element of the list
+(define (swap-first list)
+    (cons (cadr list) (cons (car list) (cddr list)))
+)
+
+; TEST FUNCTIONS
+; (load "bubble_sort.scm")
+; (bubble-sort '(4 1 3 2 82 4 2 1378 474 2 2 18 5 5812))
+; (swap-first '(5 3 2 6 1 4))
+; (sorted '(1 2 3 4))
---
+++ prac9/notes.md
@@ -0,0 +1,13 @@
+- Got stuck on CDR/CAR syntax being backwards for the longest time (sort of makes sense but example of the kind of thing I found extremely counterintuitive)
+- The bracket syntax is a nightmare to read
+  - There were so many times I got extremely unintuitive errors (such as accessing 2 variables never being equal) because of wonky brackets
+
+what
+```
+Subproblem level: 0 (this is the lowest subproblem level)
+Expression (from stack):
+    (car '("evaluated" . #t))
+There is no current environment.
+The execution history for this subproblem contains 1 reduction.
+You are now in the debugger.  Type q to quit, ? for commands.
+```
